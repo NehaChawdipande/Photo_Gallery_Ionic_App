4ce4d289f120e52874a96dbffb327d1ddd59e72b
NehaChawdipande <nehachawdipande10@gmail.com>
2021-08-17 19:33:51 +0530

    commit to master

--- photo-gallery/src/app/services/photo.service.ts
+++ photo-gallery/src/app/services/photo.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Storage } from '@capacitor/storage';
 
@@ -11,7 +11,9 @@ import { Storage } from '@capacitor/storage';
 export class PhotoService {
 
   constructor() { }
-
+  public photos: Photo[] = []; //array of photos
+  
+  
   public async addNewToGallery(){
     //take a photo
     const capturedPhoto = await Camera.getPhoto({
@@ -20,6 +22,17 @@ export class PhotoService {
       quality:100
     });
 
-    
+    //add the newly captured photos to the photos array
+    this.photos.unshift({
+      filepath:"soon..",
+      webviewPath:capturedPhoto.webPath
+    });
+
   }
+  
+}
+
+export interface Photo{
+  filepath:string;
+  webviewPath:string;
 }
