0e135588a6b845efef645acfde701e0987eca326
NehaChawdipande <nehachawdipande10@gmail.com>
2021-08-17 20:33:15 +0530

    commit to master

--- photo-gallery/src/app/services/photo.service.ts
+++ photo-gallery/src/app/services/photo.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Camera, Photo, CameraResultType, CameraSource } from '@capacitor/camera';
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Storage } from '@capacitor/storage';
 
@@ -9,10 +9,12 @@ import { Storage } from '@capacitor/storage';
   providedIn: 'root'
 })
 export class PhotoService {
-
-  constructor() { }
-  public photos: Photo[] = []; //array of photos
   
+  constructor() { }
+  public photos: Photos[] = []; //array of photos
+
+  //variable which will act as key for the store
+  private PHOTO_STORAGE : string ="photos";
   
   public async addNewToGallery(){
     //take a photo
@@ -22,17 +24,92 @@ export class PhotoService {
       quality:100
     });
 
-    //add the newly captured photos to the photos array
-    this.photos.unshift({
-      filepath:"soon..",
-      webviewPath:capturedPhoto.webPath
-    });
+    //add the newly captured photos to the photos array --for temporary storage 
+    
+    // this.photos.unshift({
+    //   filepath:"soon..",
+    //   webviewPath:capturedPhoto.webPath
+    // });
 
-  }
-  
+    //for permanent storage of pictures
+    const savedImageFile = await this.savePicture(capturedPhoto);
+    this.photos.unshift(savedImageFile);
+
+    //call yo save the photos array
+    Storage.set({
+      key:this.PHOTO_STORAGE,
+      value: JSON.stringify(this.photos)
+    });
 }
 
-export interface Photo{
+
+  //saving pictures on device permanently
+  private async savePicture(cameraPhoto: Photo)
+  {
+    //convert photo to base64 foremat required by fileSytem api to save
+
+    const base64Data = await this.readAsBase64(cameraPhoto);
+    
+    //write the file to the data directory
+    const fileName = new Date().getTime()+'.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path :fileName,
+      data: base64Data,
+      directory:Directory.Data
+    });
+
+    //use webpath to display the new image instead of base64 since it is already loaded into memory
+    return{
+      filepath: fileName,
+      webviewPath: cameraPhoto.webPath
+    };
+
+  }
+
+
+  private async readAsBase64(cameraPhoto: Photo){
+    //fetch the photo, read as blob , then convert to base64 format
+    const response = await fetch(cameraPhoto.webPath!);
+    const blob = await response.blob();
+    return await this.convertBlobToBase64(blob) as string;
+  }
+
+  convertBlobToBase64 = (blob:Blob) => new Promise((resolve,reject)=>{
+    const reader = new FileReader;
+    reader.onerror = reject;
+    reader.onload =() =>{
+      resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+
+  });
+
+
+  //loading saved photos:
+  public async loadSaved(){
+    //retrieve cached photo array data
+    const photoList = await Storage.get({
+      key: this.PHOTO_STORAGE
+    });
+    this.photos = JSON.parse(photoList.value) || [];
+
+
+    //display the photo by reading into the base64 format 
+    for(let photo of this.photos)
+    {
+        //read each saved photo's data from Filesystem
+        const readFile = await Filesystem.readFile({
+          path: photo.filepath,
+          directory: Directory.Data
+        });
+
+        //web Platforms only: Load the photo as base64 data:
+
+        photo.webviewPath =`data:image/jpeg;base64,${readFile.data}`;
+    }
+  }
+}
+export interface Photos{
   filepath:string;
   webviewPath:string;
 }
--- photo-gallery/src/app/tab2/tab2.page.ts
+++ photo-gallery/src/app/tab2/tab2.page.ts
@@ -11,7 +11,11 @@ export class Tab2Page {
 
   addPhotoToGallery(){
     this.photoService.addNewToGallery();
-    
+
+  }
+  
+  async ngOnInit(){
+    await this.photoService.loadSaved();
   }
 
 }
